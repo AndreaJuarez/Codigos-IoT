94126af3d566f471c41c1b3ca13002cd1e76cbc8
AndreaJuarez <50895566+AndreaJuarez@users.noreply.github.com>
2020-04-26 22:55:41 -0500

    Práctica 01

---
+++ practica01.ino
@@ -0,0 +1,15 @@
+void setup() {
+  // put your setup code here, to run once:
+  const int buttonPin = 2;
+  const int ledpin = 13;
+  //pinMode (n_pin, INPUT OR OUTPUT)
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(13,HIGH);
+  delay(100);
+  digitalWrite(13,LOW);
+  delay(1000);
+}
